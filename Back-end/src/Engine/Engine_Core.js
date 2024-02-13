da9471e021edb8235cac755a033daa8f937353d5
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-13 12:52:24 +0000

    let to var test

--- Back-end/src/Engine/Engine_Core.js
+++ Back-end/src/Engine/Engine_Core.js
@@ -1,17 +1,17 @@
 "use strict"
 
-let gEngine = gEngine || { };
+var gEngine = gEngine || { };
 // initialize the variable while ensuring it is not redefined
 
 gEngine.Core = (function() {
 
 // instance variable: the graphical context for drawing
-  let mGL = null;
+    var  mGL = null;
 // Accessor of the webgl context
-  let getGL = function() { return mGL; };
+   var getGL = function() { return mGL; };
 
-  let initializeWebGL = function(htmlCanvasID) {
-    let canvas = document.getElementById(htmlCanvasID);
+   var initializeWebGL = function(htmlCanvasID) {
+    var canvas = document.getElementById(htmlCanvasID);
 // Get the standard or experimental webgl and binds to the Canvas area
 // store the results to the instance variable mGL
     mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -24,14 +24,14 @@ gEngine.Core = (function() {
   };
 
   // Clears the draw area and draws one square
-   let clearCanvas = function(color) {
+   var clearCanvas = function(color) {
     mGL.clearColor(color[0], color[1], color[2], color[3]); // set color to be cleared
     mGL.clear(mGL.COLOR_BUFFER_BIT); // clear to the color previously set
   }
 
 
 // Contains the functions and variables that will be accessible.
-   let mPublic = {
+   var mPublic = {
     getGL: getGL,
     initializeWebGL : initializeWebGL,
     clearCanvas: clearCanvas
