3ff2fee7075a97463794de857af91e885e038faa
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-13 12:49:45 +0000

    var to let

--- Back-end/src/Engine/Engine_Core.js
+++ Back-end/src/Engine/Engine_Core.js
@@ -1,17 +1,17 @@
 "use strict"
 
-var gEngine = gEngine || { };
+let gEngine = gEngine || { };
 // initialize the variable while ensuring it is not redefined
 
 gEngine.Core = (function() {
 
 // instance variable: the graphical context for drawing
-  var mGL = null;
+  let mGL = null;
 // Accessor of the webgl context
-  var getGL = function() { return mGL; };
+  let getGL = function() { return mGL; };
 
-  var initializeWebGL = function(htmlCanvasID) {
-    var canvas = document.getElementById(htmlCanvasID);
+  let initializeWebGL = function(htmlCanvasID) {
+    let canvas = document.getElementById(htmlCanvasID);
 // Get the standard or experimental webgl and binds to the Canvas area
 // store the results to the instance variable mGL
     mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -24,22 +24,19 @@ gEngine.Core = (function() {
   };
 
   // Clears the draw area and draws one square
-  var clearCanvas = function(color) {
+   let clearCanvas = function(color) {
     mGL.clearColor(color[0], color[1], color[2], color[3]); // set color to be cleared
     mGL.clear(mGL.COLOR_BUFFER_BIT); // clear to the color previously set
   }
 
+
 // Contains the functions and variables that will be accessible.
-  var mPublic = {
+   let mPublic = {
     getGL: getGL,
     initializeWebGL : initializeWebGL,
     clearCanvas: clearCanvas
   };
   return mPublic;
-
-
-
-
 }());
 
 
---
+++ Back-end/src/Engine/Transform.js
@@ -0,0 +1,5 @@
+function Transform() {
+    this.mPosition = vec2.fromValues(0, 0); // translation operator
+    this.mScale = vec2.mScale = vec2.fromValues(1,1); // scaling operator
+    this.mRotationInRad = 0.0; // rotatio  in radians
+}
