6948d058deb61f3d055660bbb1042ed15af9fffc
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-20 12:05:20 +0000

    Update MyGame.js

--- Back-end/src/MyGame/MyGame.js
+++ Back-end/src/MyGame/MyGame.js
@@ -40,6 +40,7 @@ function MyGame(htmlCanvasID) {
   this.mBlueSq = new Renderable(this.mConstColorShader);
   this.mBlueSq.setColor([0.25, 0.25, 0.95, 1]);
   this.mRedSq = new Renderable(this.mConstColorShader);
+
   this.mRedSq.setColor([1, 0.25, 0.25, 1]);
   this.mTLSq = new Renderable(this.mConstColorShader);
   this.mTLSq.setColor([0.9, 0.1, 0.1, 1]);
