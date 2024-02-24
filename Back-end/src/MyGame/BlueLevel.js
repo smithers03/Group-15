0687357df5608e957bbfb83813c9c29d3567d661
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-24 16:41:33 +0000

    Level switching with coditions implemented

--- Back-end/src/MyGame/BlueLevel.js
+++ Back-end/src/MyGame/BlueLevel.js
@@ -1,73 +1,83 @@
-"use strict"
+/*
+ * File: BlueLevel.js 
+ * This is the logic of our game. 
+ */
+/*jslint node: true, vars: true */
+/*global gEngine: false, Scene: false, MyGame: false, SceneFileParser: false */
+/* find out more about jslint: http://www.jslint.com/help.html */
 
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
 
-function BlueLevel(){
+function BlueLevel() {
     // scene file name
     this.kSceneFile = "assets/BlueLevel.xml";
     // all squares
-    this.mSqSet = [];
-    // camera to view the scene
+    this.mSqSet = [];        // these are the Renderable objects
+
+    // The camera to view the scene
     this.mCamera = null;
 }
-
 gEngine.Core.inheritPrototype(BlueLevel, Scene);
 
-BlueLevel.prototype.loadScene = function(){
-    gEngine.TextFileLoader.loadTextFile(this.kSceneFile,
-        gEngine.TextFileLoader.eTextFileType.eXMLFile);
-}
-BlueLevel.prototype.initialize = function() {
+BlueLevel.prototype.loadScene = function () {
+    // load the scene file
+    gEngine.TextFileLoader.loadTextFile(this.kSceneFile, gEngine.TextFileLoader.eTextFileType.eXMLFile);
+};
+
+BlueLevel.prototype.unloadScene = function () {
+    // unload the scene flie
+    gEngine.TextFileLoader.unloadTextFile(this.kSceneFile);
+
+    var nextLevel = new MyGame();  // load the next level
+    gEngine.Core.startScene(nextLevel);
+};
+
+BlueLevel.prototype.initialize = function () {
     var sceneParser = new SceneFileParser(this.kSceneFile);
 
-    // Step A: Parse the camera
+    // Step A: Read in the camera
     this.mCamera = sceneParser.parseCamera();
 
-    // Step B: Parse for all the squares
+    // Step B: Read all the squares
     sceneParser.parseSquares(this.mSqSet);
-}
+};
 
-BlueLevel.prototype.draw = function(){
-    gEngine.Core.clearCanvas([0.9, 0.9,0.3,1.0]);
-    //activate drawing camera
+// This is the draw function, make sure to setup proper drawing environment, and more
+// importantly, make sure to _NOT_ change any state.
+BlueLevel.prototype.draw = function () {
+    // Step A: clear the canvas
+    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+
+    // Step  B: Activate the drawing Camera
     this.mCamera.setupViewProjection();
 
-    // drawing  all the squares
+    // Step  C: draw all the squares
     var i;
     for (i = 0; i < this.mSqSet.length; i++) {
         this.mSqSet[i].draw(this.mCamera.getVPMatrix());
     }
 };
 
-BlueLevel.prototype.update = function() {
-    var xform = this.mSqSet[0].getXform();
+// The Update function, updates the application state. Make sure to _NOT_ draw
+// anything from this function!
+BlueLevel.prototype.update = function () {
+    // For this very simple game, let's move the first square
+    var xform = this.mSqSet[1].getXform();
     var deltaX = 0.05;
 
-    if(gEngine.Input.isKeyPressed(gEngine.Input.keys.Right)){
-        if(xform.getXPos() > 30) // right boundh of the border
-            xform.setPosition(10,60);
+    /// Move right and swap ovre
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Right)) {
         xform.incXPos(deltaX);
-    }
-    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Up)) {
-        xform.incRotationByDegree(1);
-    }
-
-    xform = this.mSqSet[1].getXform();
-    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Down)) {
-        if (xform.getWidth() > 5)
-            xform.setSize(2, 2);
-        xform.incSizeBy(0.05);
+        if (xform.getXPos() > 30) { // this is the right-bound of the window
+            xform.setPosition(12, 60);
+        }
     }
 
-    if(gEngine.Input.isKeyPressed(gEngine.Input.keys.Left)){
+    // Step A: test for white square movement
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Left)) {
         xform.incXPos(-deltaX);
-        if(xform.getXPos() < 11) { // transition to the next level condition
+        if (xform.getXPos() < 11) { // this is the left-boundary
             gEngine.GameLoop.stop();
         }
     }
-}
-BlueLevel.prototype.unloadScene = function () {
-    gEngine.TextFileLoader.unloadTextFile(this.kSceneFile); // unloads current scene
-
-    var nextLevel = new MyGame(); // creates next level
-    gEngine.Core.startScene(nextLevel); // start scene of the next level
-}
+};
