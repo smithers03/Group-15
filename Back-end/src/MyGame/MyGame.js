c1729f01a16b558aca40df74f51d2fed98cdc2e1
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-26 22:33:00 +0000

    sprites animation complete

--- Back-end/src/MyGame/MyGame.js
+++ Back-end/src/MyGame/MyGame.js
@@ -1,13 +1,3 @@
-/*
- * File: MyGame.js
- * This is the logic of our game.
- */
-
-
-/*jslint node: true, vars: true */
-/*global gEngine: false, Scene: false, SpriteRenderable: false, Camera: false, vec2: false,
-  TextureRenderable: false, Renderable: false, SpriteAnimateRenderable: false */
-/* find out more about jslint: http://www.jslint.com/help.html */
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
