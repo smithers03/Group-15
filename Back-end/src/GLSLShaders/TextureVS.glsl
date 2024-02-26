e195ef311e4563c6a33f8c4a39d12a661a62a168
XoRoSh <74296122+XoRoSh@users.noreply.github.com>
2024-02-26 10:09:09 +0000

    refactoring

--- Back-end/src/GLSLShaders/TextureVS.glsl
+++ Back-end/src/GLSLShaders/TextureVS.glsl
@@ -4,16 +4,16 @@ attribute vec2 aTextureCoordinate;
 
 
 // texture coordinate that well map the entire image to the entire square
-varying vec2 vTextCood;
+varying vec2 vTextCoord;
 
 // to thransform the vetex position
 uniform mat4 uModelTransform;
 uniform mat4 uViewProjTransform;
 
 void main(void) {
-    gl_Positoin = uViewProjTransform * uModelTransform *
+    gl_Position = uViewProjTransform * uModelTransform *
             vec4(aSquareVertexPosition, 1.0);
 
     // the texture coordinate to the fragment shader
-    vTextCood = aTextureCoordinate;
+    vTextCoord = aTextureCoordinate;
 }
