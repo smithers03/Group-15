631716fb42a22cb183ec9ce30206ca158b38418b
HetanshM <118020914+HetanshM@users.noreply.github.com>
2024-04-09 00:32:31 +0100

    adding the end screen

---
+++ game.js
@@ -0,0 +1,7 @@
+function showGameOverScreen() {
+    document.getElementById("gameOverScreen").style.display = "flex";
+}
+
+
+// This should be called when your game logic determines the game is over.
+showGameOverScreen();
